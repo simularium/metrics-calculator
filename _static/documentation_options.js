52125d2a9b1f59e1e5c30dbeeff5e40f59c8bbf4
blairlyons <blair208@gmail.com>
2022-09-21 22:59:07 +0000

    Deploying to gh-pages from @ simularium/metrics-calculator@3d043a669673ef57873230606cd61abd7f0e7bb7 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g70aa5d0',
+    VERSION: '0.1.dev1+g3d043a6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
